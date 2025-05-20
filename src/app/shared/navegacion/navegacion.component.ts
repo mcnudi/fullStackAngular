8e556023c528ab85716ca3a32651550a96d146bf
mcnudi <mcnudi@hotmail.com>
2025-05-20 11:39:18 +0200

    subida nav y rutas iniciales

---
+++ src/app/shared/navegacion/navegacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navegacion',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navegacion.component.html',
+  styleUrl: './navegacion.component.css'
+})
+export class NavegacionComponent {
+
+}
