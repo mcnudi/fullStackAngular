9222c6da949ecd68907eb3639bed63371979017e
mcnudi <mcnudi@hotmail.com>
2025-05-21 09:05:55 +0200

    Auto stash before merge of "landing" and "origin/develop"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingComponent } from '../landing/landing.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet,LandingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
