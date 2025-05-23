08c8e9c1a93e03d48a5a9ba044ee6047b3383606
mcnudi <mcnudi@hotmail.com>
2025-05-23 17:14:12 +0200

    creacion landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from '../landing/landing.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet,LandingComponent],
+  imports: [LandingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { LandingImgComponent } from '../landing-img/landing-img.component';
+
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [LandingImgComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit {
+  // {
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    });
 
+    const elements = document.querySelectorAll('.slide-up-section');
+    elements.forEach(el => observer.observe(el));
+  }
 }
