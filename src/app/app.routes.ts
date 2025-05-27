4363e3d11904d28874c9bc7ab06965817410c47e
carlossr15 <csobrado@ucm.es>
2025-05-27 22:20:02 +0200

    Se ha creado la página de recuperación de contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { PasswordRecoveryComponent } from './pages/passwordRecovery/passwordRecovery.component';
+
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'passwordRecovery', component: PasswordRecoveryComponent },
   { path: '**', redirectTo: 'home' },
 ];
