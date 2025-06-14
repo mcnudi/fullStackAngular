bfd07d22fc0cc2618254c83e68abd29dce6efd36
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-06-14 12:09:18 +0200

    Aplicacion de estilos dinamicos al link activo de la barra de navegacion

--- src/app/shared/applayout/applayout.component.ts
+++ src/app/shared/applayout/applayout.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-applayout',
-  imports: [MatSidenavModule, RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    RouterOutlet,
+    MatDividerModule,
+    MatListModule,
+    RouterLink,
+    RouterModule,
+  ],
   templateUrl: './applayout.component.html',
   styleUrl: './applayout.component.css',
 })
