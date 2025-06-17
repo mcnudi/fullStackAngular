543757d839738afdfe3e64e1af1f7c8868e151d2
Sergio <sgarci21@xtec.cat>
2025-06-17 16:25:12 +0200

    Cambios en rutinas list

--- src/app/pages/rutinas-list/rutinas-list.component.ts
+++ src/app/pages/rutinas-list/rutinas-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rutinas-list',
@@ -14,5 +17,18 @@ export class RutinasListComponent {
   async ngOnInit() {
     this.username = this.routerL.snapshot.paramMap.get('usuario') || '';
     
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RutinaService {
+  private baseUrl = 'http://localhost:3000/rutinas'; // Ajusta si tienes proxy
+
+  constructor(private http: HttpClient) {}
+
+  getRutinasByUser(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/user/${userId}`);
   }
 }
