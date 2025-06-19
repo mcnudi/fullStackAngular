0a186ee9ea39a60883994116efab1864f3074daf
Sergio Bravo <s.bravoga1995@gmail.com>
2025-06-19 19:03:48 +0200

    Incorporación de formulario Actividad y Form-info-actividad

---
+++ src/app/pages/dashboard/form-info-activity/form-info-activity.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-form-info-activity',
+  templateUrl: './form-info-activity.component.html',
+  styleUrls: ['./form-info-activity.component.css']
+})
+export class FormInfoActivityComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
+
+ @Input() actividad: any;
+  @Output() cerrar = new EventEmitter<void>();
+
+  cerrarModal() {
+    this.cerrar.emit();
+  }
+}
