46e5ce93581c3d3ca2e76603ceffaf1e10832af6
Pepe Puertas Alonso <pepepuertasalonso4@gmail.com>
2025-06-22 17:34:51 +0200

    Integrando backend con front

---
+++ src/app/interfaces/irecomendedActivity.ts
@@ -0,0 +1,8 @@
+export interface RecommendedActivities {
+  title: string;
+  description: string;
+  day_of_week: string;
+  day: number;
+  start_time: string;
+  end_time: string;
+}
--- src/app/services/rutina.service.ts
+++ src/app/services/rutina.service.ts
@@ -2,19 +2,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Irutina } from '../interfaces/irutina';
 import { Observable } from 'rxjs';
+import { RecommendedActivities } from '../interfaces/irecomendedActivity';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RutinaService {
-
-  private http = inject(HttpClient)
-  private baseURL: string = "http://localhost:3000/api/rutinas"
+  private http = inject(HttpClient);
+  private baseURL: string = 'http://localhost:3000/api/rutinas';
 
   insertRutina(rutina: Irutina): Observable<Irutina> {
-    return this.http.post<Irutina>(
-      `${this.baseURL}`, rutina
-    );
+    return this.http.post<Irutina>(`${this.baseURL}`, rutina);
   }
 
   modificarRutina(rutina: Irutina): Observable<Irutina> {
@@ -24,13 +22,21 @@ export class RutinaService {
   // obtener rutinas por ID de usuario (requiere token)
   getRutinasByUser(userId: number, token: string): Observable<Irutina[]> {
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
 
-    return this.http.get<Irutina[]>(`${this.baseURL}/user/${userId}`, { headers });
+    return this.http.get<Irutina[]>(`${this.baseURL}/user/${userId}`, {
+      headers,
+    });
   }
 
-  obtenerRutinas(id:number):Observable<Irutina[]>{
+  obtenerRutinas(id: number): Observable<Irutina[]> {
     return this.http.get<Irutina[]>(`${this.baseURL}/${id}`);
+  }
+
+  generarRutina(id: number): Observable<RecommendedActivities[]> {
+    return this.http.get<RecommendedActivities[]>(
+      `${this.baseURL}/generate/${id}`
+    );
   }
 }
