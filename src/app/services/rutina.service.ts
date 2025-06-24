ad77937f712519bb2de230a02228f467f9dff4de
Pepe Puertas Alonso <pepepuertasalonso4@gmail.com>
2025-06-24 15:36:51 +0200

    Descarga de pdf

--- src/app/services/rutina.service.ts
+++ src/app/services/rutina.service.ts
@@ -4,17 +4,14 @@ import { Irutina } from '../interfaces/irutina.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RutinaService {
-
-  private http = inject(HttpClient)
-  private baseURL: string = "http://localhost:3000/api/rutinas"
+  private http = inject(HttpClient);
+  private baseURL: string = 'http://localhost:3000/api/rutinas';
 
   insertRutina(rutina: Irutina): Observable<Irutina> {
-    return this.http.post<Irutina>(
-      `${this.baseURL}`, rutina
-    );
+    return this.http.post<Irutina>(`${this.baseURL}`, rutina);
   }
 
   modificarRutina(rutina: Irutina): Observable<Irutina> {
@@ -24,18 +21,26 @@ export class RutinaService {
   // obtener rutinas por ID de usuario (requiere token)
   getRutinasByUser(userId: number, token: string): Observable<Irutina[]> {
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     });
 
-    return this.http.get<Irutina[]>(`${this.baseURL}/user/${userId}`, { headers });
+    return this.http.get<Irutina[]>(`${this.baseURL}/user/${userId}`, {
+      headers,
+    });
   }
 
-  obtenerRutinas(id:number):Observable<Irutina[]>{
+  obtenerRutinas(id: number): Observable<Irutina[]> {
     return this.http.get<Irutina[]>(`${this.baseURL}/${id}`);
   }
 
-  obtenerRutinaVersiones(id:number):Observable<Irutina[]>{
-    console.log("la rutina es:",id);
+  obtenerRutinaVersiones(id: number): Observable<Irutina[]> {
+    console.log('la rutina es:', id);
     return this.http.get<Irutina[]>(`${this.baseURL}/version/${id}`);
+  }
+
+  descargarPdf(rutinaId: number) {
+    return this.http.get(`${this.baseURL}/generatePdf/${rutinaId}`, {
+      responseType: 'blob', 
+    });
   }
 }
