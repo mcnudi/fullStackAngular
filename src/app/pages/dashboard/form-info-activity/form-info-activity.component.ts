a1c7c48df44ddee56b910f0ad5e55977bcd0540b
Sergio Bravo <s.bravoga1995@gmail.com>
2025-06-29 12:18:02 +0200

    Incorporación de la Categoria en el Formulario de la Actividad (Ver)

--- src/app/pages/dashboard/form-info-activity/form-info-activity.component.ts
+++ src/app/pages/dashboard/form-info-activity/form-info-activity.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Activity } from '../../../interfaces/iactivity.interface';
+import { Category } from '../../../interfaces/icategory.interface';
 
 @Component({
   selector: 'app-form-info-activity',
@@ -6,13 +8,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./form-info-activity.component.css']
 })
 export class FormInfoActivityComponent implements OnInit {
-  @Input() actividad: any;
+  @Input() actividad: Activity | undefined;
   @Output() cerrar = new EventEmitter<void>();
+  @Input() categorias: Category[] | undefined;
 
-
+  categoryName: string = '';
 
   ngOnInit(): void {
-
+    if (this.actividad?.activity_categories_id && this.categorias) {
+      const categoria = this.categorias.find(
+        (cat) => cat.id === this.actividad!.activity_categories_id
+      );
+      this.categoryName = categoria?.category_name || '';
+    }
   }
 
   cerrarModal() {
