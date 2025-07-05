48a637d407b5d09432cb6275c7fd7948f0349064
Sergio <sgarci21@xtec.cat>
2025-07-05 17:11:34 +0200

    eliminacion console.log

--- src/app/pages/rutinas-list/rutinas-list.component.ts
+++ src/app/pages/rutinas-list/rutinas-list.component.ts
@@ -24,19 +24,15 @@ export class RutinasListComponent implements OnInit {
   authService = inject(AuthService);
 
   filtroBusqueda: string = '';
-
   currentPage: number = 1;
   pageSize: number = 5;
 
   ngOnInit(): void {
-    console.log('🔥 ngOnInit ejecutado');
-
     const userId = this.authService.getDecodedToken().id;
     const token = localStorage.getItem('token');
     this.username = this.authService.getUserName();
 
     if (!userId || !token) {
-      console.warn('Faltan userId o token en localStorage');
       return;
     }
 
@@ -44,24 +40,17 @@ export class RutinasListComponent implements OnInit {
       Authorization: `Bearer ${token}`,
     });
 
-    console.log(
-      '✅ Lanzando GET a:',
-      `${this.API_URL}/api/rutinas/user/${userId}`
-    );
-
     this.http
       .get<any[]>(`${this.API_URL}/api/rutinas/user/${userId}`, {
         headers,
       })
       .subscribe({
         next: (data) => {
-          console.log(
-            '📦 Rutinas recibidas (detalle):',
-            JSON.stringify(data, null, 2)
-          );
           this.rutinas = data;
         },
-        error: (err) => console.error('❌ Error al obtener rutinas:', err),
+        error: () => {
+          
+        },
       });
   }
 
